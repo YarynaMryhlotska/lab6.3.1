90ab5e0a5557132624dd95f346f583a3246c30e9
YarynaMryhlotska <89984528+YarynaMryhlotska@users.noreply.github.com>
2021-11-06 20:27:01 +0200

    22

--- lab6.3.1.cpp
+++ lab6.3.1.cpp
@@ -2,16 +2,46 @@
 #include <iomanip>
 #include <time.h>
 using namespace std;
+template <typename T >
+void Create(T* a, const T n, const T Low, const T High)
+{
+    for (int i = 0; i < n; i++)
+        a[i] = Low + rand() % (High - Low + 1);
+}
+
 void Create(int* a, const int n, const int Low, const int High)
 {
     for (int i = 0; i < n; i++)
         a[i] = Low + rand() % (High - Low + 1);
 }
+
+
+template <typename T >
+void Print(T* a, const T n)
+{
+    for (int i = 0; i < n; i++)
+        cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+    cout << endl;
+}
+
+
 void Print(int* a, const int n)
 {
     for (int i = 0; i < n; i++)
         cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
     cout << endl;
+}
+
+
+
+template <typename T >
+const T& Sum(T* a, const T n)
+{
+    int S = 0;
+
+    for (int i = 0; i < n; i++)
+        S += a[i];
+    return S;
 }
 int Sum(int* a, const int n)
 {
